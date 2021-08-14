ce99bc853ace230e6e422d8c3f115080ea871275
somator <torysoma@gmail.com>
2021-08-14 14:10:09 -0700

    Initiate default bitboards

--- public/script.js
+++ public/script.js
@@ -89,8 +89,10 @@ class Game {
         this.board = board;
         this.perspective = perspective;
         this.fen = fen;
+        this.bitboards = {};
         this.annotateSquares();
         this.fillBoardFromFen();
+        this.initDefaultBitboards();
     }
 
     // Assign rank and file to square elements according to perspective
@@ -176,6 +178,21 @@ class Game {
                 }
             }
         }
+    }
+
+    initDefaultBitboards() {
+        this.bitboards['P'] = new Bitboard(0x00000000, 0x0000ff00);
+        this.bitboards['N'] = new Bitboard(0x00000000, 0x00000042);
+        this.bitboards['B'] = new Bitboard(0x00000000, 0x00000024);
+        this.bitboards['R'] = new Bitboard(0x00000000, 0x00000081);
+        this.bitboards['Q'] = new Bitboard(0x00000000, 0x00000010);
+        this.bitboards['K'] = new Bitboard(0x00000000, 0x00000008);
+        this.bitboards['p'] = new Bitboard(0x00ff0000, 0x00000000);
+        this.bitboards['n'] = new Bitboard(0x42000000, 0x00000000);
+        this.bitboards['b'] = new Bitboard(0x24000000, 0x00000000);
+        this.bitboards['r'] = new Bitboard(0x81000000, 0x00000000);
+        this.bitboards['q'] = new Bitboard(0x10000000, 0x00000000);
+        this.bitboards['k'] = new Bitboard(0x08000000, 0x00000000);
     }
 }
 
