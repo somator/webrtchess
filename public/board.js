f1f8ec16835ef8e2cc0821b02e97c4c3cd340a66
somator <torysoma@gmail.com>
2021-08-20 23:43:15 -0700

    Add empty move pattern functions for each piece type

--- public/board.js
+++ public/board.js
@@ -10,22 +10,51 @@ class Board {
         return;
     }
 
-    findMoves() {
+    findMoves(piece, pos) {
+        switch (piece.type) {
+            case 'pawn':
+                moves = pawnPattern()
+                break;
+            case 'knight':
+                moves = knightPattern();
+                break;
+            case 'bishop':
+                moves = bishopPattern();
+                break;
+            case 'rook':
+                moves = rookPattern();
+                break;
+            case 'queen':
+                moves = queenPattern();
+                break;
+            case 'king':
+                moves = kingPattern();
+                break;
+        }
+        return moves;
+    }
+
+    pawnPattern() {
         return;
     }
 
-    initDefaultBitboards() {
-        this.bitboards['P'] = new Bitboard(0x00000000, 0x0000ff00);
-        this.bitboards['N'] = new Bitboard(0x00000000, 0x00000042);
-        this.bitboards['B'] = new Bitboard(0x00000000, 0x00000024);
-        this.bitboards['R'] = new Bitboard(0x00000000, 0x00000081);
-        this.bitboards['Q'] = new Bitboard(0x00000000, 0x00000010);
-        this.bitboards['K'] = new Bitboard(0x00000000, 0x00000008);
-        this.bitboards['p'] = new Bitboard(0x00ff0000, 0x00000000);
-        this.bitboards['n'] = new Bitboard(0x42000000, 0x00000000);
-        this.bitboards['b'] = new Bitboard(0x24000000, 0x00000000);
-        this.bitboards['r'] = new Bitboard(0x81000000, 0x00000000);
-        this.bitboards['q'] = new Bitboard(0x10000000, 0x00000000);
-        this.bitboards['k'] = new Bitboard(0x08000000, 0x00000000);
+    knightPattern() {
+        return;
+    }
+
+    bishopPattern() {
+        return;
+    }
+
+    rookPattern() {
+        return;
+    }
+
+    queenPattern() {
+        return;
+    }
+
+    kingPattern() {
+        return;
     }
 }
