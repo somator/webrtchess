02c0075f7155edf15c6cd8f7592df2ba17deb15f
somator <torysoma@gmail.com>
2021-08-21 22:35:52 -0700

    initDefaultBitboards no longer returns an object, accesses this.bitboards directly

--- public/board.js
+++ public/board.js
@@ -71,24 +71,23 @@ function bitboardToAn(bitboard) {
 
 export class Board {
     constructor() {
-        this.bitboards = this.initDefaultBitboards();
+        this.bitboards = {};
+        this.initDefaultBitboards();
     }
 
     initDefaultBitboards() {
-        bitboards = {};
-        bitboards['P'] = new Bitboard(0x00000000, 0x0000ff00);
-        bitboards['N'] = new Bitboard(0x00000000, 0x00000042);
-        bitboards['B'] = new Bitboard(0x00000000, 0x00000024);
-        bitboards['R'] = new Bitboard(0x00000000, 0x00000081);
-        bitboards['Q'] = new Bitboard(0x00000000, 0x00000010);
-        bitboards['K'] = new Bitboard(0x00000000, 0x00000008);
-        bitboards['p'] = new Bitboard(0x00ff0000, 0x00000000);
-        bitboards['n'] = new Bitboard(0x42000000, 0x00000000);
-        bitboards['b'] = new Bitboard(0x24000000, 0x00000000);
-        bitboards['r'] = new Bitboard(0x81000000, 0x00000000);
-        bitboards['q'] = new Bitboard(0x10000000, 0x00000000);
-        bitboards['k'] = new Bitboard(0x08000000, 0x00000000);
-        return bitboards;
+        this.bitboards['P'] = new Bitboard(0x00000000, 0x0000ff00);
+        this.bitboards['N'] = new Bitboard(0x00000000, 0x00000042);
+        this.bitboards['B'] = new Bitboard(0x00000000, 0x00000024);
+        this.bitboards['R'] = new Bitboard(0x00000000, 0x00000081);
+        this.bitboards['Q'] = new Bitboard(0x00000000, 0x00000010);
+        this.bitboards['K'] = new Bitboard(0x00000000, 0x00000008);
+        this.bitboards['p'] = new Bitboard(0x00ff0000, 0x00000000);
+        this.bitboards['n'] = new Bitboard(0x42000000, 0x00000000);
+        this.bitboards['b'] = new Bitboard(0x24000000, 0x00000000);
+        this.bitboards['r'] = new Bitboard(0x81000000, 0x00000000);
+        this.bitboards['q'] = new Bitboard(0x10000000, 0x00000000);
+        this.bitboards['k'] = new Bitboard(0x08000000, 0x00000000);
     }
 
     move() {
