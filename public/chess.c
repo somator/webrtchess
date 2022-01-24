5b5e2c0f7b2dc6c9e4229c77f4b568ed730ce5fb
somator <torysoma@gmail.com>
2022-01-23 20:44:37 -0800

    Boilerplate find_moves c function

--- public/chess.c
+++ public/chess.c
@@ -1,11 +1,32 @@
-// hello.c
+// chess.c
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include "emscripten.h"
 
-int version() {
-    return 1;
-}
+typedef unsigned long long U64;
 
-char *hello_world() {
-    return "hello, world!\n";
+enum Color {
+    WHITE = 0,
+    BLACK = 1,
+};
+
+enum Type {
+    PAWN = 0,
+    KNIGHT = 1,
+    BISHOP = 2,
+    ROOK = 3,
+    QUEEN = 4,
+    KING = 5,
+};
+
+char *movesPtr;
+
+char *find_moves(char start_pos[]) {
+    // allocate space for 42 bytes (2 chars for rank and file times 21 maximum potential moves per piece)
+    movesPtr = calloc(42, sizeof(char));
+    movesPtr[0] = 'e';
+    movesPtr[1] = '4';
+
+    return movesPtr;
 }
